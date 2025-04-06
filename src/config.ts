d6d89d8fbf06638e2a645643ca59689fb7fe4bc8
JackReaperCZ <domcapour@seznam.cz>
2025-04-06 18:50:52 +0200

    Config edit

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,9 @@ export const config = {
     issuer: 'http://localhost:5148', //Backend domain
     audience: 'http://localhost:3000', //Frontend domain
     secretKey: 'your-secret-key-here', //Secret key for JWT
+    //DB
+    DB_HOST: '127.0.0.1',
+    DB_USER: 'root',
+    DB_PASSWORD: '',
+    DATABASE: 'kiosek',
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
+import {config} from "../config";
 
 // Database configuration
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'kiosek',
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
